de0033a88d8d2ec98f65373127575063af2bc4b6
BiCffQ <31585109+BiCffQ@users.noreply.github.com>
2020-08-25 08:41:37 +0000

    Create Where_s_my_partner.java

---
+++ Where_s_my_partner.java
@@ -0,0 +1,9 @@
+public class humans{
+	public humans(String name){
+		System.out.println("你的对象的名字是 : " + name ); 
+	}
+	public static void main(String[] args){
+		humans boyfriend = new humans("阿强");
+		humans girlfriend= new humans("阿珍");
+	}
+}
